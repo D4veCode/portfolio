1c2603265328d1437f292bb48fdfc4f460f0ebba
David Serrada <davidasb.developer@gmail.com>
2020-02-29 23:33:00 -0400

    Cambios en titulo y experiencia

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 <template>
   <div id="app">
      <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">
-        <span class="d-block d-lg-none">¡Bienvenido!</span>
+      <span class="navbar-brand js-scroll-trigger" href="#page-top" >
         <span class="d-none d-lg-block">
-          <img class="img-fluid img rounded-circle mx-auto mb-2" src="./assets/img/ds trans 2.png" alt="">
+          <img class="img-fluid img rounded-circle mx-auto mb-2" src="./assets/img/ds trans 2.png" alt="logo">
         </span>
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+      </span>
+     
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav">
           <li class="nav-item">
@@ -96,7 +93,7 @@ export default {
       },
       experiences:[
         {job: 'Asistente Drupal', company:'Strategy Plaza', description:'Asistente de la empresa Strategy Plaza, actualiza, crea y modifica sitios web basados en Drupal 8.', date: 'Ene 2018 - Agosto 2018'},
-        {job: 'Desarrollador Backend', company: 'YEiPii', description: 'Desarrollador Backend, mis tareas fueron refactorizar algunos componentes en el backend hecho en NodeJS e implementar nuevas funciones.', date: 'Jun 2019- Agosto 2019'}
+        {job: 'Desarrollador Backend', company: 'YEiPii', description: 'Desarrollador Backend, mis tareas fueron refactorizar algunos componentes en el backend hecho en NodeJS e implementar nuevas funciones.', date: 'Jun 2019 - Agosto 2019'}
       
       ],
       education:[{school:'Universidad Católica Andrés Bello', degree:'8vo Semestre de Ingeniería Infomática', date:'Sept 2015 - Presente'},
